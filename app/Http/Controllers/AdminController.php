e1caa3c8de2f9ae3337db5deba13fae10d967855
Soyamstha <shrestha.soyam111@gmail.com>
2025-06-27 15:35:25 +0545

    link not returning

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\BillResource;
+use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserResource;
 use App\Models\Bill;
+use App\Models\User;
+use App\Notifications\FcmNotification;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -11,28 +15,46 @@ class AdminController extends Controller
     function view_bills()
     {
         $user = auth()->user();
-        if($user->is_admin == 1)
-        {
-            $pagination =Bill::orderBy('created_at', 'desc')->paginate(6);
+        if ($user->is_admin == 1) {
+            $pagination = Bill::orderBy('created_at', 'desc')->paginate(6);
             return BillResource::collection($pagination);
-        }
-        else
-        {
-            return response()->json(['message'=>'you are not admin']);
+        } else {
+            return response()->json(['message' => 'you are not admin']);
         }
     }
-    function update(Request $request,$id)
+    function update(Request $request, $id)
     {
         $bill = Bill::find($id);
         $request->validate([
-            'data'=>'required|in:Accepted,Rejected'
+            'data' => 'required|in:Accepted,Rejected'
         ]);
-        if(!$bill)
-        {
-            return response()->json(['message'=>'Bill not found']);
+        if (!$bill) {
+            return response()->json(['message' => 'Bill not found']);
         }
-        $bill->status=$request->data;
-        $bill->save();
-        return response()->json(['message'=>'update successful']);
+        $bill->update([
+            'status' => $request->data,
+            'feedback'=>$request->feedback
+        ]);
+        $user= $bill->user;
+        // $this->sendNotification($user);
+        return response()->json(['message' => 'update successful']);
+    }
+    function all_user()
+    {
+       $users = User::paginate(6);
+        // dd($users);
+        // return $users->links()
+        $users = UserResource::collection($users);
+        return apiSuccessResponse($users, 'User list fetched successfully');
+    }
+
+    public function sendNotification($user)
+    {
+        $deviceToken =$user->FCM_token;
+        $title = 'Welcome';
+        $message = 'Hello test message';
+        $notification = new FcmNotification($title, $message, $deviceToken);
+        $notification->toFcm(null);
+        return response()->json(['status' => 'Notification Sent']);
     }
 }
